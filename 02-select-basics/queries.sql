538b91a23deaa6c7534bb61710f0c277495308fc
busesrkya <buse@example.com>
2026-07-19 18:59:16 +0000

    Add basic SELECT query examples

---
+++ 02-select-basics/queries.sql
@@ -0,0 +1,10 @@
+-- Topic: Basic SELECT queries
+
+-- Select every column and row
+SELECT * FROM students;
+
+-- Select only specific columns
+SELECT first_name, last_name FROM students;
+
+-- Select every column from the grades table
+SELECT * FROM grades;
